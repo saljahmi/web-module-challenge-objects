acf4d45268565e4863587434ed319b06aceeb33f
saljahmi <saljahmi@gmail.com>
2020-07-16 16:05:10 -0400

    completed task 3

--- index.js
+++ index.js
@@ -59,6 +59,7 @@ const reviews = [{name: "Daniela", rating: 5, feedback:"Beautiful atmosphere and
 
 /* Task 3: Console.log just Julius' feedback */
 
+console.log(reviews[5].feedback);
 
 /* Task 4: Add a new rating with your (fictitious) opinions of the restaurant in the same format as the reviews above. */
 
